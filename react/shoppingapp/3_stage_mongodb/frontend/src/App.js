62fb81782dd80fe652b7f046ec1858a9efb5f85c
Erno Hentonen <erno.hentonen@opiframe.com>
2020-12-10 13:47:52 +0200

    mongodb

---
+++ react/shoppingapp/3_stage_mongodb/frontend/src/App.js
@@ -0,0 +1,246 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import {Switch,Route,Redirect} from 'react-router-dom';
+import ShoppingList from './components/ShoppingList';
+import ShoppingForm from './components/ShoppingForm';
+import LoginForm from './components/LoginForm';
+import Navbar from './components/Navbar';
+
+class App extends React.Component {
+	
+	constructor(props) {
+		super(props);
+		this.state = {
+			list:[],
+			isLogged:false,
+			token:""
+		}
+	}
+	
+	componentDidMount() {
+		if(sessionStorage.getItem("state")) {
+			let state = JSON.parse(sessionStorage.getItem("state"));
+			this.setState(state, () => {
+				if(this.state.isLogged) {
+					console.log(this.state);
+					this.getList();
+				}
+			})
+		}
+		
+	}
+	
+	saveToStorage = () => {
+		sessionStorage.setItem("state",JSON.stringify(this.state));
+	}
+	
+	clearState = () => {
+		this.setState({
+			isLogged:false,
+			list:[],
+			token:""
+		}, () => {
+			this.saveToStorage();
+		})
+	}
+	
+	//Login api
+	
+	register = (user) => {
+		let request = {
+			method:"POST",
+			mode:"cors",
+			headers:{"Content-type":"application/json"},
+			body:JSON.stringify(user)
+		}
+		fetch("/register",request).then(response => {
+			if(response.ok) {
+				alert("Register success!");
+			} else {
+				console.log("Server responded with a status:",response.status);
+			}
+		}).catch(error => {
+			console.log("Server responded with an error. Reason:",error);
+		});
+	}
+
+	login = (user) => {
+		let request = {
+			method:"POST",
+			mode:"cors",
+			headers:{"Content-type":"application/json"},
+			body:JSON.stringify(user)
+		}
+		fetch("/login",request).then(response => {
+			if(response.ok) {
+				response.json().then(data => {
+					this.setState({
+						isLogged:true,
+						token:data.token
+					},() => {
+						this.saveToStorage();
+						this.getList();
+					})
+				}).catch(error => {
+					console.log("Failed to parse JSON. Error:",error);
+				})
+			} else {
+				console.log("Server responded with a status:",response.status);
+			}
+		}).catch(error => {
+			console.log("Server responded with an error. Reason:",error);
+		});
+	}	
+	
+	logout = () => {
+		let request = {
+			method:"POST",
+			mode:"cors",
+			headers:{"Content-type":"application/json",
+					token:this.state.token}
+		}
+		fetch("/logout",request).then(response => {
+			if(response.ok) {
+				this.clearState();
+			} else {
+				console.log("Server responded with status:",response.status);
+				this.clearState();
+			}
+		}).catch(error => {
+			console.log("Server responded with an error:",error);
+		});
+	}
+	//Shopping api
+	
+	getList = (query) => {
+		let request = {
+			method:"GET",
+			mode:"cors",
+			headers:{"Content-type":"application/json",
+					token:this.state.token}
+		}
+		let url = "/api/shopping";
+		if(query) {
+			url = url+"?type="+query
+		}
+		fetch(url,request).then(response => {
+			if(response.ok) {
+				response.json().then(data => {
+					this.setState({
+						list:data
+					}, () => {
+						this.saveToStorage();
+					}) 
+				}).catch(error => {
+					console.log("Parsing of JSON failed. Reason:",error);
+				});
+			} else {
+				if(response.status === 403) {
+					this.clearState();
+					console.log("Session expired. Logging you out!");
+				}
+				console.log("Server responded with a status:"+response.status);
+			}
+		}).catch(error => {
+			console.log("Server responded with an error. Reason:",error);
+		});
+	}
+	
+	addToList = (item) => {
+		let request = {
+			method:"POST",
+			mode:"cors",
+			headers:{"Content-type":"application/json",
+					token:this.state.token},
+			body:JSON.stringify(item)
+		}
+		fetch("/api/shopping",request).then(response => {
+			if(response.ok) {
+				this.getList();
+			} else {
+				if(response.status === 403) {
+					this.clearState();
+					console.log("Session expired. Logging you out!");
+				}
+				console.log("Server responded with a status:"+response.status);
+			}
+		}).catch(error => {
+			console.log("Server responded with an error. Reason:",error);
+		});
+	}
+	
+	removeFromList = (id) => {
+		let request = {
+			method:"DELETE",
+			mode:"cors",
+			headers:{"Content-type":"application/json",
+					token:this.state.token}
+		}
+		fetch("/api/shopping/"+id,request).then(response => {
+			if(response.ok) {
+				this.getList();
+			} else {
+				if(response.status === 403) {
+					this.clearState();
+					console.log("Session expired. Logging you out!");
+				}
+				console.log("Server responded with a status:"+response.status);
+			}
+		}).catch(error => {
+			console.log("Server responded with an error. Reason:",error);
+		});		
+	}
+	
+	editItem = (item) => {
+		let request = {
+			method:"PUT",
+			mode:"cors",
+			headers:{"Content-type":"application/json",
+					token:this.state.token},
+			body:JSON.stringify(item)
+		}
+		fetch("/api/shopping/"+item.id,request).then(response => {
+			if(response.ok) {
+				this.getList();
+			} else {
+				if(response.status === 403) {
+					this.clearState();
+					console.log("Session expired. Logging you out!");
+				}
+				console.log("Server responded with a status:"+response.status);
+			}
+		}).catch(error => {
+			console.log("Server responded with an error. Reason:",error);
+		});		
+	}
+	
+	
+    render() {
+		return (
+			<div className="App">
+				<Navbar isLogged={this.state.isLogged} logout={this.logout}/>
+				<hr/>
+				<Switch>
+					<Route exact path="/" render={() => this.state.isLogged ?
+						(<Redirect to="/list"/>) :
+						(<LoginForm register={this.register} login={this.login}/>)
+					}/>
+					<Route path="/list" render={() => this.state.isLogged ?
+						(<ShoppingList list={this.state.list}
+							removeFromList={this.removeFromList}
+							editItem={this.editItem}
+							getList={this.getList}/>) :
+						(<Redirect to="/"/>)
+					}/>
+					<Route path="/form" render={() => this.state.isLogged ?
+						(<ShoppingForm addToList={this.addToList}/>) :
+						(<Redirect to="/"/>)
+					}/>
+				</Switch>
+			</div>
+		);
+    }
+}
+
+export default App;
---
+++ react/shoppingapp/3_stage_mongodb/frontend/src/components/ShoppingList.js
@@ -0,0 +1,108 @@
+import React from 'react';
+import {Table,Button} from 'semantic-ui-react';
+import Row from './Row';
+import RemoveRow from './RemoveRow';
+import EditRow from './EditRow';
+
+export default class ShoppingList extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			removeIndex:-1,
+			editIndex:-1,
+			search:""
+		}
+	}
+	
+	searchByType = (event) => {
+		this.props.getList(this.state.search);
+		this.setState({
+			search:""
+		})
+	}
+	
+	onChange = (event) => {
+		let state = {};
+		state[event.target.name] = event.target.value;
+		this.setState(state);
+	}
+	
+	handleRemoveButton = (id) => {
+		for(let i=0;i<this.props.list.length;i++) {
+			if(id === this.props.list[i]._id) {
+				this.setState({
+					removeIndex:i,
+					editIndex:-1
+				})
+			}
+		}
+	}
+
+	handleEditButton = (id) => {
+		for(let i=0;i<this.props.list.length;i++) {
+			if(id === this.props.list[i]._id) {
+				this.setState({
+					removeIndex:-1,
+					editIndex:i
+				})
+			}
+		}
+	}	
+	cancel = () => {
+		this.setState({
+			removeIndex:-1,
+			editIndex:-1
+		})
+	}
+	
+	removeFromList = (id) => {
+		this.props.removeFromList(id);
+		this.cancel();
+	}
+	
+	editItem = (item) => {
+		this.props.editItem(item);
+		this.cancel();
+	}
+
+	render() {
+		let items = this.props.list.map((item,index) => {
+			if(index === this.state.removeIndex) {
+				return (<RemoveRow item={item} key={item._id} removeFromList={this.removeFromList} cancel={this.cancel}/>)
+			}
+			if(index === this.state.editIndex) {
+				return (<EditRow item={item} key={item._id} editItem={this.editItem} cancel={this.cancel}/>)
+			}
+			
+			return (	
+				<Row item={item} key={item._id} handleRemoveButton={this.handleRemoveButton}
+				handleEditButton={this.handleEditButton}/>
+			)
+		})
+		return(
+		<div>
+			<label htmlFor="search">Search by type:</label>
+			<input type="text"
+					name="search"
+					onChange={this.onChange}
+					value={this.state.search}/>
+			<Button onClick={this.searchByType} style={{marginLeft:10}}>Search</Button>
+			<Table striped>
+				<Table.Header>
+					<Table.Row>
+						<Table.HeaderCell>Item type</Table.HeaderCell>
+						<Table.HeaderCell>Count</Table.HeaderCell>
+						<Table.HeaderCell>Price</Table.HeaderCell>
+						<Table.HeaderCell>Remove</Table.HeaderCell>
+						<Table.HeaderCell>Edit</Table.HeaderCell>
+					</Table.Row>
+				</Table.Header>
+				<Table.Body>
+				{items}
+				</Table.Body>
+			</Table>
+		</div>
+		)		
+	}
+}
