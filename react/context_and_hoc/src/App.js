192b718a6975049a2ab7a5b2f504ecc124821eca
Erno Hentonen <erno.hentonen@opiframe.com>
2021-01-19 12:31:30 +0200

    context and hoc

--- react/context_and_hoc/src/App.js
+++ react/context_and_hoc/src/App.js
@@ -1,25 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {Switch,Route,Redirect} from 'react-router-dom';
+import StateManager from './statemanager/StateManager';
+import ShoppingForm from './components/ShoppingForm';
+import ShoppingList from './components/ShoppingList';
+import Navbar from './components/Navbar';
+import LoginForm from './components/LoginForm';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+	render() {
+		return (
+			<div className="App">
+				<Navbar/>
+				<hr/>
+				<Switch>
+					<Route exact path="/" render={() => this.props.isLogged ?
+					(<Redirect to="/list"/>) :
+					(<LoginForm/>)
+					}/>
+					<Route path="/list" render={() => this.props.isLogged ?
+					(<ShoppingList/>) :
+					(<Redirect to="/"/>)
+					}/>
+					<Route path="/form" render={() => this.props.isLogged ?
+					(<ShoppingForm/>) :
+					(<Redirect to="/"/>)
+					}/>
+				</Switch>
+			</div>
+		);
+	}
 }
 
-export default App;
+export default StateManager(App);
--- react/context_and_hoc/src/index.js
+++ react/context_and_hoc/src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'semantic-ui-css/semantic.min.css';
+import {BrowserRouter} from 'react-router-dom';
+import StateProvider from './statemanager/context/StateProvider';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+  <BrowserRouter>
+	  <StateProvider>
+		<App />
+	  </StateProvider>
+  </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
