0dd5f91549b3d268c537d073ad8e4a0be767e805
Erno Hentonen <erno.hentonen@opiframe.com>
2021-01-19 12:53:18 +0200

    hoclogger 1

---
+++ react/shoppingapp/5_stage_hoclogger/frontend/src/components/Navbar.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import {List,Header} from 'semantic-ui-react';
+import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
+import {logout} from '../actions/loginActions';
+
+class Navbar extends React.Component {
+	
+	render() {
+		let header = <Header>Shopping App</Header>
+		if(this.props.loading) {
+			header = <Header>Loading ...</Header>
+		}
+		if(this.props.error) {
+			header = <Header>{this.props.error}</Header>
+		}
+		let navStyle = {
+			height:100,
+			backgroundColor:"lightblue"
+		}
+		if(this.props.isLogged) {
+			return (
+				<div style={navStyle}>
+					{header}
+					<List>
+						<List.Item><Link to="/list">Shopping List</Link></List.Item>
+						<List.Item><Link to="/form">Add to list</Link></List.Item>
+						<List.Item><Link to="/" onClick={() => this.props.dispatch(logout(this.props.token))}>Logout</Link></List.Item>
+					</List>
+				</div>
+			)
+		} else {
+			return (
+				<div style={navStyle}>
+					{header}
+				</div>
+			)
+		}
+	}
+}
+
+const mapStateToProps = (state) => {
+	let error = "";
+	if(state.shopping.error) {
+		error = state.shopping.error
+	}
+	if(state.login.error) {
+		error = state.login.error
+	}
+	return {
+		isLogged:state.login.isLogged,
+		token:state.login.token,
+		loading:state.login.loading,
+		error:error
+	}
+}
+
+export default connect(mapStateToProps)(Navbar);
